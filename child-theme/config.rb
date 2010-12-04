442c292090b8673403f8a2173750bd287460c5e3
Janne Asmala <janne@asmala.name>
2010-12-04 16:37:35 +0200

    Added child theme scaffold. Updated widget code.

---
+++ child-theme/config.rb
@@ -0,0 +1,9 @@
+# Compass configuration options
+
+require 'html5-boilerplate'
+require '8020-boilerplate-compass'
+
+css_dir =    'stylesheets'
+images_dir = 'stylesheets/images'
+sass_dir =   'stylesheets/src'
+output_style = :compressed
