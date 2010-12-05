1312e661426ce0af7585b2f0213475655401458d
Janne Asmala <janne@asmala.name>
2010-12-05 17:02:05 +0200

    Added a wrapper for blog posts. Added some more placeholder selectors to the child theme stylesheet.

--- parts/loop.php
+++ parts/loop.php
@@ -1,30 +1,32 @@
-<?php if (!have_posts()) : ?>
-	<article class="page error404 not-found">
-		<header class="entry-header text-content">
-			<h1 class="entry-title">Not Found</h1>
-		</header>
-		<section class="entry-content text-content">
-			<p>Apologies, but no results were found for the requested
-				 archive.</p>
-		</section>
-	</article>
-<?php endif; ?>
+<div id="articles">
+	<?php if (!have_posts()) : ?>
+		<article class="page error404 not-found">
+			<header class="entry-header text-content">
+				<h1 class="entry-title">Not Found</h1>
+			</header>
+			<section class="entry-content text-content">
+				<p>Apologies, but no results were found for the requested
+					 archive.</p>
+			</section>
+		</article>
+	<?php endif; ?>
 
-<?php while (have_posts()) : the_post(); ?>
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<header class="entry-header text-content">
-			<h1 class="entry-title">
-				<a href="<?php the_permalink(); ?>" class="permalink" rel="bookmark"><?php the_title(); ?></a>
-			</h1>
-			<p class="post-meta">
-				<?php the_author(); ?> |
-				<?php the_date(); ?> |
-				<?php comments_popup_link(); ?>
-			</p>
-		</header>
-		<section class="entry-content text-content">
-			<?php the_content('Continue reading &rarr;'); ?>
-		</section>
-		<?php is_single() ? comments_template() : ''; ?>
-	</article>
-<?php endwhile; ?>
+	<?php while (have_posts()) : the_post(); ?>
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<header class="entry-header text-content">
+				<h1 class="entry-title">
+					<a href="<?php the_permalink(); ?>" class="permalink" rel="bookmark"><?php the_title(); ?></a>
+				</h1>
+				<p class="post-meta">
+					<?php the_author(); ?> |
+					<?php the_date(); ?> |
+					<?php comments_popup_link(); ?>
+				</p>
+			</header>
+			<section class="entry-content text-content">
+				<?php the_content('Continue reading &rarr;'); ?>
+			</section>
+			<?php is_single() ? comments_template() : ''; ?>
+		</article>
+	<?php endwhile; ?>
+</div>
